cd0666d8dfcdec96d4d075b0ee11712f4267da6a
Rafael Conde <rafahari@me.com>
2017-04-15 20:43:38 +0100

    We have the initial calc being rendered

--- SalaryCalculator/SalaryCalculator/ViewController.swift
+++ SalaryCalculator/SalaryCalculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var myTextFieldLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func typingInTextField(_ sender: Any) {
+        var theNumberThatWeGot = Double(myTextFieldLabel.text!)
+        if let theNumberThatWeGot = theNumberThatWeGot {
+            let numberConverted = simpleCalc(numberToCalc: theNumberThatWeGot)
+            
+            print(numberConverted)
+            resultsLabel.text = String(numberConverted)
+            
+        } else {
+            // TODO: Replace passing a literal "0.0" string to the label, by the actual Double value
+            theNumberThatWeGot = 0.0
+            myTextFieldLabel.text = "0.0"
+        }
+    }
+    
+    func simpleCalc(numberToCalc: Double) -> Double {
+        return (
+            numberToCalc * 2.0
+        )
+    }
 }
 
