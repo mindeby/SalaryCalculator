f71426377328373a46cab9ee865e742663f4ccbe
mindeby <debora.marisa.ribeiro@gmail.com>
2017-05-25 18:34:34 +0100

    Fix indentation

--- SalaryCalculator/SalaryCalculator/ViewController.swift
+++ SalaryCalculator/SalaryCalculator/ViewController.swift
@@ -19,8 +19,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     var selectedDD: Double = 0.0
     var salarioBase: Double = 0.0
     var salarioLiquido: Double = 0.0
-
-
     
 
     override func viewDidLoad() {
@@ -69,41 +67,41 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         salarioBruto = self.salarioBase + (self.salarioBase * self.selectedDD)
         
         switch salarioBruto {
-        case 0...610 :
-            irsPercentage = 0.0
-        case 610...618:
-            irsPercentage = 0.018
-        case 618...640:
-            irsPercentage = 0.05
-        case 640...678:
-            irsPercentage = 0.06
-        case 678...730:
-            irsPercentage = 0.075
-        case 730...805:
-            irsPercentage = 0.085
-        case 805...912:
-            irsPercentage = 0.11
-        case 912...993:
-            irsPercentage = 0.125
-        case 993...1053:
-            irsPercentage = 0.135
-        case 1053...1130:
-            irsPercentage = 0.145
-        case 1130...1211:
-            irsPercentage = 0.155
-        case 1211...1307:
-            irsPercentage = 0.165
-        case 1307...1408:
-            irsPercentage = 0.175
-        case 1408...1545:
-            irsPercentage = 0.185
-        case 1545...1691:
-            irsPercentage = 0.200
-        case 1691...1849:
-            irsPercentage = 0.215
-        case 1849...1955:
-            irsPercentage = 0.225
-        default: break
+            case 0...610 :
+                irsPercentage = 0.0
+            case 610...618:
+                irsPercentage = 0.018
+            case 618...640:
+                irsPercentage = 0.05
+            case 640...678:
+                irsPercentage = 0.06
+            case 678...730:
+                irsPercentage = 0.075
+            case 730...805:
+                irsPercentage = 0.085
+            case 805...912:
+                irsPercentage = 0.11
+            case 912...993:
+                irsPercentage = 0.125
+            case 993...1053:
+                irsPercentage = 0.135
+            case 1053...1130:
+                irsPercentage = 0.145
+            case 1130...1211:
+                irsPercentage = 0.155
+            case 1211...1307:
+                irsPercentage = 0.165
+            case 1307...1408:
+                irsPercentage = 0.175
+            case 1408...1545:
+                irsPercentage = 0.185
+            case 1545...1691:
+                irsPercentage = 0.200
+            case 1691...1849:
+                irsPercentage = 0.215
+            case 1849...1955:
+                irsPercentage = 0.225
+            default: break
         }
 
         self.salarioLiquido = salarioBruto - (salarioBruto * ssPercentage) - (salarioBruto * irsPercentage) + (diasSubsidio * diariaSubsidioA)
@@ -124,14 +122,14 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         let stringToShow: String
         
         switch rawItem {
-        case 0.0:
-            stringToShow = "0%"
-        case 0.5:
-            stringToShow = "50%"
-        case 1.0:
-            stringToShow = "100%"
-        default:
-            stringToShow = "Something went wrong"
+            case 0.0:
+                stringToShow = "0%"
+            case 0.5:
+                stringToShow = "50%"
+            case 1.0:
+                stringToShow = "100%"
+            default:
+                stringToShow = "Something went wrong"
         }
         
         return stringToShow
