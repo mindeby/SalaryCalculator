3ebd781b93be93d938313b9b2be1c83b1e7df9a7
mindeby <debora.marisa.ribeiro@gmail.com>
2017-05-25 18:31:36 +0100

    Calc IRS and SalarioLiquido

--- SalaryCalculator/SalaryCalculator/ViewController.swift
+++ SalaryCalculator/SalaryCalculator/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var resultsLabel: UILabel!
@@ -16,8 +17,76 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     let ddPickerData = [0.0, 0.5, 1.0]
     var selectedDD: Double = 0.0
-    
+    var salarioBruto: Double = 0.0
+    var irsPercentage: Double = 0.0
     var salarioBase: Double = 0.0
+
+
+    
+// SALARIO BRUTO
+    func calculateSalarioBruto() {
+        self.salarioBruto = (self.salarioBase + (self.salarioBase * self.selectedDD))
+
+    }
+    
+// IRS PERCENTAGE
+    func calculateIrsPercentage() {
+        
+        switch salarioBruto {
+            case 0...610 :
+                self.irsPercentage = 0.0
+            case 610...618:
+                self.irsPercentage = 1.8
+            case 618...640:
+                self.irsPercentage = 5.0
+            case 640...678:
+                self.irsPercentage = 6.0
+            case 678...730:
+                self.irsPercentage = 7.5
+            case 730...805:
+                self.irsPercentage = 8.5
+            case 805...912:
+                self.irsPercentage = 11.0
+            case 912...993:
+                self.irsPercentage = 12.5
+            case 993...1053:
+                self.irsPercentage = 13.5
+            case 1053...1130:
+                self.irsPercentage = 14.5
+            case 1130...1211:
+                self.irsPercentage = 15.5
+            case 1211...1307:
+                self.irsPercentage = 16.5
+            case 1307...1408:
+                self.irsPercentage = 17.5
+            case 1408...1545:
+                self.irsPercentage = 18.5
+            case 1545...1691:
+                self.irsPercentage = 20.0
+            case 1691...1849:
+                self.irsPercentage = 21.5
+            case 1849...1955:
+                self.irsPercentage = 22.5
+            default: break
+        }
+        
+//        return irsPercentage //FIX  THIS
+        
+    }
+    
+    //  outras variaveis
+    
+    let SsPercentage: Double = 0.11
+    var diasSubsidio: Double = 0.0
+    var diariaSubsidioA: Double = 7.23
+    var salarioLiquido: Double = 0.0
+    
+    // CALCULAR SALARIO LIQUIDO
+    
+    func calculateSalarioLiquido() {
+        self.salarioLiquido = self.salarioBruto - (self.salarioBruto * self.SsPercentage) - (self.salarioBruto * self.irsPercentage) + (self.diasSubsidio * self.diariaSubsidioA)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,22 +98,23 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
     @IBAction func typingInTextField(_ sender: Any) {
         let theNumberThatWeGot = Double(myTextFieldLabel.text!)
         
         if let theNumberThatWeGot = theNumberThatWeGot {
             self.salarioBase = theNumberThatWeGot
-            calculateSalarioLiquid()
+            calculateIrsPercentage()
+            calculateSalarioLiquido()
             
         } else {
             self.salarioBase = 0.0
-            calculateSalarioLiquid()
+            calculateSalarioLiquido()
         }
-    }
-    
-    func calculateSalarioLiquid() {
-        let valueToShow: Double = (self.salarioBase + (self.salarioBase * self.selectedDD))
+        
+        let valueToShow: Double = salarioLiquido.self
         self.resultsLabel.text = String(valueToShow)
     }
     
@@ -61,14 +131,14 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         let stringToShow: String
         
         switch rawItem {
-            case 0.0:
-                stringToShow = "0%"
-            case 0.5:
-                stringToShow = "50%"
-            case 1.0:
-                stringToShow = "100%"
-            default:
-                stringToShow = "Debs fix this"
+        case 0.0:
+            stringToShow = "0%"
+        case 0.5:
+            stringToShow = "50%"
+        case 1.0:
+            stringToShow = "100%"
+        default:
+            stringToShow = "Something went wrong"
         }
         
         return stringToShow
@@ -76,8 +146,19 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         self.selectedDD = self.ddPickerData[row]
-        calculateSalarioLiquid()
+        calculateSalarioLiquido()
         print(self.selectedDD)
     }
+    
+
 }
+
+
+
+
+
+
+
+
+
 
--- SalaryCalculator/SalaryCalculator/myHelpers.swift
+++ SalaryCalculator/SalaryCalculator/myHelpers.swift
@@ -6,44 +6,51 @@
 //  Copyright © 2017 Debora Ribeiro. All rights reserved.
 //
 
-import Foundation
+/*import Foundation
 
-//switch salarioBruto {
-//    case 0...610 :
-//        0.0
-//    case 610...618:
-//        1.8
-//    case 618...640:
-//        5.0
-//    case 640...678:
-//        6.0
-//    case 678...730:
-//        7.5
-//    case 730...805:
-//        8.5
-//    case 805...912:
-//        11.0
-//    case 912...993:
-//        12.5
-//    case 993...1053:
-//        13.5
-//    case 1053...1130:
-//        14.5
-//    case 1130...1211:
-//        15.5
-//    case 1211...1307:
-//        16.5
-//    case 1307...1408:
-//        17.5
-//    case 1408...1545:
-//        18.5
-//    case 1545...1691:
-//        20.0
-//    case 1691...1849:
-//        21.5
-//    case 1849...1955:
-//        22.5
-//    default: ""
+var salarioBruto: Double = 0.0
+
+var irsPercentage: Double = 0.0
+
+switch salarioBruto {
+    case 0...610 :
+        irsPercentage = 0.0
+    case 610...618:
+        irsPercentage = 1.8
+    case 618...640:
+        irsPercentage = 5.0
+    case 640...678:
+        irsPercentage = 6.0
+    case 678...730:
+        irsPercentage = 7.5
+    case 730...805:
+        irsPercentage = 8.5
+    case 805...912:
+        irsPercentage = 11.0
+    case 912...993:
+        irsPercentage = 12.5
+    case 993...1053:
+        irsPercentage = 13.5
+    case 1053...1130:
+        irsPercentage = 14.5
+    case 1130...1211:
+        irsPercentage = 15.5
+    case 1211...1307:
+        irsPercentage = 16.5
+    case 1307...1408:
+        irsPercentage = 17.5
+    case 1408...1545:
+        irsPercentage = 18.5
+    case 1545...1691:
+        irsPercentage = 20.0
+    case 1691...1849:
+        irsPercentage = 21.5
+    case 1849...1955:
+        irsPercentage = 22.5
+    default:
+        irsPercentage = 0.0
+
+}
 //}
 //
-//label.text = salarioBruto - (salarioBruto * SS) - (salarioBruto * escalaoIRS) + subsidioAlimentacao
+//label.text = salarioBruto - (salarioBruto * SS) - (salarioBruto * escalaoIRS) + subsidioAlimentacao */
